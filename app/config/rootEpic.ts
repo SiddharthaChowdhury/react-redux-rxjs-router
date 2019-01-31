bb36c3f7c9b870f9f2be02d46960934ed653d2cf
Austin4Silvers <siddharthac6@gmail.com>
2019-02-01 04:50:16 +0530

    Fix type error following
    TS2345: Argument of type 'Epic<Action<any>, Action<any>, void, any>' is not assignable to parameter of type 'Epic<Action<any>, Action<any>, IState, any>'.   Type 'void' is not assignable to type 'IState'.

--- app/config/rootEpic.ts
+++ app/config/rootEpic.ts
@@ -2,6 +2,6 @@ import { combineEpics } from 'redux-observable';
 import { epicCheckLogin } from '../features/auth/epicAuth';
 
 // List all the epics here
-export default combineEpics(
+export default combineEpics<Action<any>, Action<any>, IState>(
     epicCheckLogin,
 );
